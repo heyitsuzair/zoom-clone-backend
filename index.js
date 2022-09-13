dc35fcc6d00bb4dfda940887904350869a53ea17
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-14 00:59:17 +0500

    Final Push

--- index.js
+++ index.js
@@ -14,25 +14,23 @@ app.use(cors());
 app.use(express.static());
 
 app.get("*", function (req, res) {
-  res.send("Running!");
+  io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("callEnded");
+    });
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+      io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    socket.on("answerCall", (data) => {
+      io.to(data.to).emit("callAccepted", data.signal);
+    });
+  });
 });
 
 const PORT = process.env.PORT || 5000;
-
-io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
-
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
-
-  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-  });
-
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
-});
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
